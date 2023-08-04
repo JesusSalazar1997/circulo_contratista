fa2b506e5a3eb0960e53f2127dbb20b2f1c19345
JesusSalazar1997 <epastrabajando@gmail.com>
2023-08-03 22:21:41 -0600

    Changes conexion

--- config/clienteAxios.jsx
+++ config/clienteAxios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    // baseURL: `http://deployndeliver-001-site1.btempurl.com/api`
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
+    baseURL: `http://deployndeliver-001-site1.btempurl.com/api`
+    // baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`
 })
 
 export default clienteAxios;
